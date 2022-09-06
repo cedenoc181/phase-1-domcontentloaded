423372cd22efc01cc85111d2f28d74bf3f3f54e3
Christian <cedenoc181@gmail.com>
2022-09-06 12:26:36 -0400

    commit tree 6759f4a888dda0148b5d233c5fbc392b46fb6b7c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// const p = document.getElementById('text');
+
+// function loadDomFirst (){
+//     document.getElementById('text').textContent = 'This is really cool!';
+// }
+
+// document.addEventListener('DOMContentLoaded', loadDomFirst );
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log( document.getElementById('text').textContent = 'This is really cool!');
+  });
+
